c15c41f06885b081aeb0147aee361c4bea4320d6
Ibrahim Ahmed <aebrahem743@gmail.com>
2024-12-05 20:00:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ollama_API_Testing;
+using OllamaClient;
+using OllamaClient.Extensions;
+using System.Text;
+using System.Threading;
+
+namespace Ollama_API_Testing
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            //ollamaSharp method
+            var chatProgram = new OllamaChatProgram("http://localhost:11434");
+            await chatProgram.Initialize();
+            await chatProgram.RunChat();
+
+
+
+
+
+
+            //// ollamaClient method 
+            //const string modelName = "phi3"; // Example model name
+
+            //// Create an instance of OllamaClient
+            //var ollamaClient = new OllamaClient(modelName);
+
+            //// Start the chat loop
+            //await ollamaClient.StartChatLoop();
+        }
+    }
+}
+
+
+
